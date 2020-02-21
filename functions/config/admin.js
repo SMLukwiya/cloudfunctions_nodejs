4a4b5410b649d0ec27415b7b21ee8bd660ad4fab
SMLukwiya <sundaymorganl@gmail.com>
2020-02-21 15:22:52 +0300

    updates to serviceAccount

--- functions/config/admin.js
+++ functions/config/admin.js
@@ -4,8 +4,8 @@ let serviceAccount = require("../serviceAccount.json");
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
-  databaseURL: "https://socialapp-69777.firebaseio.com",
-  storageBucket: "socialapp-69777.appspot.com"
+  databaseURL: yourfirebase database url,
+  storageBucket: your storage bucket name
 });
 
 const db = admin.firestore();
